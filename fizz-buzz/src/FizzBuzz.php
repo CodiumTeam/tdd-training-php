540fd06b6daaf871844902d84c0d87178c02b1da
Luis Rovirosa <luisrovirosa@gmail.com>
2015-05-19 07:11:11 +0200

    FizzBuzz

---
+++ fizz-buzz/src/FizzBuzz.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FizzBuzz;
+
+class FizzBuzz
+{
+
+    /**
+     * @return bool
+     */
+    public function changeMe()
+    {
+        return true;
+    }
+}
---
+++ fizz-buzz/test/FizzBuzzTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FizzBuzz\Test;
+
+use FizzBuzz\FizzBuzz;
+
+class FizzBuzzTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function change_me()
+    {
+        $fizzBuzz = new FizzBuzz();
+        $this->assertTrue($fizzBuzz->changeMe());
+    }
+}
