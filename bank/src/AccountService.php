da488cae6035451017f055731316fd2b75146a4e
Luis Rovirosa <luisrovirosa@gmail.com>
2015-05-20 08:43:56 +0200

    KataBank

---
+++ bank/src/AccountService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace KataBank;
+
+class AccountService
+{
+    /**
+     * Add an amount to the account.
+     * Do not return any value
+     * @param int $amount
+     */
+    public function deposit($amount)
+    {
+    }
+
+    /**
+     * Remove an amount from the account.
+     * Do not return any value
+     * @param int $amount
+     */
+    public function withdraw($amount)
+    {
+    }
+
+    /**
+     * Print the statements containing all the transactions in the console
+     * Do not return any value
+     */
+    public function printStatements()
+    {
+    }
+}
---
+++ bank/test/KataBankTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace KataBank\Test;
+
+use KataBank\AccountService;
+use Prophecy\PhpUnit\ProphecyTestCase;
+
+class KataBankTest extends ProphecyTestCase
+{
+    /** @test */
+    public function should_print_statements_containing_all_transactions()
+    {
+        $consoleProphecy = $this->prophesize('KataBank\Console');
+
+        $account = new AccountService();
+        $account->deposit(1000);
+        $account->withdraw(100);
+        $account->deposit(500);
+
+        $account->printStatements();
+
+        $consoleProphecy->printLine("DATE | AMOUNT | BALANCE")->shouldBeCalled();
+        $consoleProphecy->printLine("10/04/2014 | 500 | 1400")->shouldBeCalled();
+        $consoleProphecy->printLine("02/04/2014 | -100 | 900")->shouldBeCalled();
+        $consoleProphecy->printLine("01/04/2014 | 1000 | 1000")->shouldBeCalled();
+    }
+}
