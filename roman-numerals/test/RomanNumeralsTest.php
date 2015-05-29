0181eef1d6f0a699dcce9cabae2a42539e11c85d
Luis Rovirosa <luisrovirosa@gmail.com>
2015-05-29 19:54:44 +0200

    Roman numerals renamed variable

--- roman-numerals/test/RomanNumeralsTest.php
+++ roman-numerals/test/RomanNumeralsTest.php
@@ -6,9 +6,10 @@ use RomanNumerals\RomanNumerals;
 
 class RomanNumeralsTest extends \PHPUnit_Framework_TestCase
 {
-  /** @test */
-  public function change_me() {
-    $fizzBuzz = new RomanNumerals();
-    $this->assertTrue($fizzBuzz->changeMe());
-  }
+    /** @test */
+    public function change_me()
+    {
+        $romanNumerals = new RomanNumerals();
+        $this->assertTrue($romanNumerals->changeMe());
+    }
 }
