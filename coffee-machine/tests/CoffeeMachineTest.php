92334f4b802a229606c69ad8b3695fa5859623e1
Luis Rovirosa <luisrovirosa@gmail.com>
2017-05-25 08:31:18 +0200

    first version of coffee machine

---
+++ coffee-machine/tests/CoffeeMachineTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CoffeeMachine\Tests;
+
+use CoffeeMachine\CoffeeMachine;
+
+class CoffeeMachineTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function xxx() {
+        new CoffeeMachine();
+        $this->assertEquals(true, true);
+    }
+}
