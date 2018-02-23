9f3457ae85582d4a7a6a9aceb3a7f4d91ea51338
Luis Rovirosa <luisrovirosa@gmail.com>
2018-02-23 14:28:10 +0100

    adds the drinkmaker

---
+++ coffee-machine/src/DrinkMaker.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CoffeeMachine;
+
+class DrinkMaker
+{
+    public function execute($command)
+    {
+        // you don't have to modify this method
+    }
+}
--- fizz-buzz/test/FizzBuzzTest.php
+++ fizz-buzz/test/FizzBuzzTest.php
@@ -14,4 +14,64 @@ class FizzBuzzTest extends TestCase
         $fizzBuzz = new FizzBuzz();
         $this->assertTrue($fizzBuzz->changeMe());
     }
+
+    /** @test */
+    public function it_should_get_list_of_100_items()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $list = $fizzBuzz->getList();
+
+        $this->assertEquals(100, count($list));
+    }
+
+    /** @test */
+    public function it_should_get_list_gives_first_number_1()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $list = $fizzBuzz->getList();
+
+        $this->assertEquals(1, $list[0]);
+    }
+
+    /** @test */
+    public function it_should_get_list_gives_97th_number_98()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $list = $fizzBuzz->getList();
+
+        $this->assertEquals(98, $list[97]);
+    }
+
+    /** @test */
+    public function it_should_convert_third_item_to_Fizz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $list = $fizzBuzz->getList();
+
+        $this->assertEquals("Fizz", $list[2]);
+    }
+
+    /** @test */
+    public function it_should_convert_fifth_item_to_Buzz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $list = $fizzBuzz->getList();
+
+        $this->assertEquals("Buzz", $list[4]);
+    }
+
+    /** @test */
+    public function it_should_convert_15th_item_to_FizzBuzz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $list = $fizzBuzz->getList();
+
+        $this->assertEquals("FizzBuzz", $list[14]);
+    }
 }
