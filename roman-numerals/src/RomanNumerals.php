592e87981fd5badd41f88034073cb0236c733da3
Luis Rovirosa Mairlot <luis@codium.team>
2024-09-11 15:52:02 +0200

    [roman-numerals] refactor extract to constant

--- roman-numerals/src/RomanNumerals.php
+++ roman-numerals/src/RomanNumerals.php
@@ -4,26 +4,25 @@ namespace RomanNumerals;
 
 class RomanNumerals
 {
+    private const CONVERSION = [
+        1000 => "M",
+        900 => "CM",
+        500 => "D",
+        400 => "CD",
+        100 => "C",
+        90 => "XC",
+        50 => "L",
+        40 => "XL",
+        10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I",
+    ];
 
     public function convertToRoman(int $decimal): string
     {
-        $conversion = [
-            1000 => "M",
-            900 => "CM",
-            500 => "D",
-            400 => "CD",
-            100 => "C",
-            90 => "XC",
-            50 => "L",
-            40 => "XL",
-            10 => "X",
-            9 => "IX",
-            5 => "V",
-            4 => "IV",
-            1 => "I",
-        ];
-
-        foreach ($conversion as $number => $roman) {
+        foreach (self::CONVERSION as $number => $roman) {
             if ($decimal >= $number) {
                 return $roman . $this->convertToRoman($decimal - $number);
             }
