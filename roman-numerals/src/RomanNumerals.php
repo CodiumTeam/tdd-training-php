e077320a2f024f4c9542ab02b20187c75755b4a2
Luis Rovirosa Mairlot <luis@codium.team>
2024-09-11 15:39:44 +0200

    [roman-numerals] increase duplicity to increase symmetry

--- roman-numerals/src/RomanNumerals.php
+++ roman-numerals/src/RomanNumerals.php
@@ -8,16 +8,16 @@ class RomanNumerals
     public function convertToRoman(int $decimal): string
     {
         if ($decimal >= 10) {
-            return "X";
+            return "X" . $this->convertToRoman($decimal - 10);
         }
         if ($decimal >= 9) {
-            return "IX";
+            return "IX" . $this->convertToRoman($decimal - 9);
         }
         if ($decimal >= 5) {
             return "V" . $this->convertToRoman($decimal - 5);
         }
         if ($decimal >= 4) {
-            return "IV";
+            return "IV" . $this->convertToRoman($decimal - 4);
         }
         if ($decimal >= 1) {
             return "I" . $this->convertToRoman($decimal - 1);
