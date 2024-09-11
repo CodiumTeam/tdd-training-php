1251668f0a63a93658a046d319d28597fc2bf071
Luis Rovirosa Mairlot <luis@codium.team>
2024-09-11 15:43:32 +0200

    [roman-numerals] reduced duplicity

--- roman-numerals/src/RomanNumerals.php
+++ roman-numerals/src/RomanNumerals.php
@@ -9,24 +9,16 @@ class RomanNumerals
     {
         $conversion = [
             10 => "X",
+            9 => "IX",
+            5 => "V",
+            4 => "IV",
+            1 => "I",
         ];
 
         foreach ($conversion as $number => $roman) {
             if ($decimal >= $number) {
                 return $roman . $this->convertToRoman($decimal - $number);
             }
-        }
-        if ($decimal >= 9) {
-            return "IX" . $this->convertToRoman($decimal - 9);
-        }
-        if ($decimal >= 5) {
-            return "V" . $this->convertToRoman($decimal - 5);
-        }
-        if ($decimal >= 4) {
-            return "IV" . $this->convertToRoman($decimal - 4);
-        }
-        if ($decimal >= 1) {
-            return "I" . $this->convertToRoman($decimal - 1);
         }
         return "";
     }
