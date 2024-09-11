ad04dc8e0b0a29ffed1bddd0f38b35e4dfa8e3e9
Luis Rovirosa Mairlot <luis@codium.team>
2024-09-11 15:38:44 +0200

    [roman-numerals] reduce duplicity

--- roman-numerals/src/RomanNumerals.php
+++ roman-numerals/src/RomanNumerals.php
@@ -13,20 +13,14 @@ class RomanNumerals
         if ($decimal === 9) {
             return "IX";
         }
-        if ($decimal >= 6) {
+        if ($decimal >= 5) {
             return "V" . $this->convertToRoman($decimal - 5);
-        }
-        if ($decimal === 5) {
-            return "V";
         }
         if ($decimal === 4) {
             return "IV";
         }
-        if ($decimal >= 2) {
+        if ($decimal >= 1) {
             return "I" . $this->convertToRoman($decimal - 1);
-        }
-        if ($decimal === 1) {
-            return "I";
         }
         return "";
     }
