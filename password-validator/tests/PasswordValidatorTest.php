5deae95b2707e6f1e730bb88597d0b50295552f0
Luis Rovirosa Mairlot <luis@codium.team>
2025-09-29 16:29:16 +0200

    [password-validator] update PHP to 8.3 and simplify Docker usage

--- password-validator/tests/PasswordValidatorTest.php
+++ password-validator/tests/PasswordValidatorTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace PasswordValidator\Test;
 
 use PasswordValidator\PasswordValidator;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class PasswordValidatorTest extends TestCase
 {
-    /** @test */
+
+    #[Test]
     public function change_me(): void
     {
         $passwordValidator = new PasswordValidator();
